e0f9433968a4d9a9487874928fb5be974b2477b0
swethakeerthipati <swethakeerthipati@gmail.com>
2023-07-19 11:08:38 +0200

    Setup database and Product Schema

---
+++ db/models/Product.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const productsSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productsSchema);
+
+export default Product;
