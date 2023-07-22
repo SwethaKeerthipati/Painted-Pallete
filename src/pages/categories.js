44edb9e01d1a9fe2306ca3f5ea84ee825ab4e519
swethakeerthipati <swethakeerthipati@gmail.com>
2023-07-22 10:55:26 +0200

    Fix build error

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,19 +1,19 @@
 import dbConnect from "../../db/connect";
 
-export default async function handler(req, res) {
-  try {
-    const { db } = await dbConnect();
-    const categories = await db.collection("categories").find({}).toArray();
-    res.status(200).json(categories);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-}
+// export default async function handler(req, res) {
+//   try {
+//     const { db } = await dbConnect();
+//     const categories = await db.collection("categories").find({}).toArray();
+//     res.status(200).json(categories);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// }
 
-// const CategoriesPage = () => {
-//   // Your page content here
-//   return <div>Categories Page</div>;
-// };
+const CategoriesPage = () => {
+  // Your page content here
+  return <div>Categories Page</div>;
+};
 
-// export default CategoriesPage;
+export default CategoriesPage;
