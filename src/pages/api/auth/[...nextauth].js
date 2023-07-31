cb7243ab724f74aad6d6932b912cbd3a9d322656
swethakeerthipati <swethakeerthipati@gmail.com>
2023-07-31 09:08:33 +0200

    Modify Next_auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,66 +1,66 @@
-// import NextAuth from "next-auth";
-// import GithubProvider from "next-auth/providers/github";
-// import GoogleProvider from "next-auth/providers/google";
-
-// export const authOptions = {
-//   // Configure one or more authentication providers
-//   providers: [
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//       secret: process.env.SECRET,
-//     }),
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       secret: process.env.SECRET,
-//     }),
-//     // ...add more providers here
-//   ],
-//   callbacks: {
-//     async jwt({ token, account }) {
-//       // Persist the OAuth access_token to the token right after signin
-//       if (account) {
-//         token.accessToken = account.access_token;
-//       }
-//       return token;
-//     },
-//     async session({ session, token, user }) {
-//       // Send properties to the client, like an access_token from a provider.
-//       session.accessToken = token.accessToken;
-//       return session;
-//     },
-//   },
-// };
-
-// export default NextAuth(authOptions);
-
 import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      secret: process.env.SECRET,
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      secret: process.env.SECRET,
     }),
     // ...add more providers here
   ],
-
-  pages: {
-    signIn: "/auth/signin",
-  },
-
   callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
     async session({ session, token, user }) {
-      session.user.username = session.user.name
-        .split(" ")
-        .join("")
-        .toLocaleLowerCase();
-
-      session.user.uid = token.sub;
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
       return session;
     },
   },
-});
+};
+
+export default NextAuth(authOptions);
+
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// export default NextAuth({
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+
+//   pages: {
+//     signIn: "/auth/signin",
+//   },
+
+//   callbacks: {
+//     async session({ session, token, user }) {
+//       session.user.username = session.user.name
+//         .split(" ")
+//         .join("")
+//         .toLocaleLowerCase();
+
+//       session.user.uid = token.sub;
+//       return session;
+//     },
+//   },
+// });
